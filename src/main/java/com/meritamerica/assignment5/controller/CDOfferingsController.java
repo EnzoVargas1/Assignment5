8a299bd1ac705f64c8d6bae4a25e1ee62d2f4647
ashwin nirmul da21-01-6311 <ashwin.metropcs@gmail.com>
2021-04-18 14:28:13 -0400

    mRemoved test_cases.txt and added the import for @RestController.

--- src/main/java/com/meritamerica/assignment5/controller/CDOfferingsController.java
+++ src/main/java/com/meritamerica/assignment5/controller/CDOfferingsController.java
@@ -5,35 +5,39 @@ import java.util.ArrayList;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.meritamerica.assignment5.exceptions.InvalidRequestException;
 import com.meritamerica.assignment5.models.CDOffering;
 import com.meritamerica.assignment5.models.MeritBank;
 
 @RestController
-public class CDOfferingsController {
-	
+public class CDOfferingsController
+{
+
 	ArrayList<CDOffering> cdOfferings = new ArrayList<CDOffering>();
-	
-	@PostMapping(value ="/CDOfferings")
-	public CDOffering createCDOffering(@RequestBody CDOffering cdOffering)throws InvalidRequestException {
-		
-		if(cdOffering.getInterestRate() <= 0 || cdOffering.getInterestRate() >= 1) {
-			throw new  InvalidRequestException("Invalid Request");
+
+	@PostMapping( value = "/CDOfferings" )
+	public CDOffering createCDOffering( @RequestBody CDOffering cdOffering ) throws InvalidRequestException
+	{
+
+		if( cdOffering.getInterestRate() <= 0 || cdOffering.getInterestRate() >= 1 )
+		{
+			throw new InvalidRequestException( "Invalid Request" );
 		}
-		if(cdOffering.getTerm() < 1) {
-			throw new  InvalidRequestException("Invalid Request");
+		if( cdOffering.getTerm() < 1 )
+		{
+			throw new InvalidRequestException( "Invalid Request" );
 		}
-		cdOfferings.add(cdOffering);
-		MeritBank.setCDOfferings(cdOfferings.toArray(new CDOffering[0]));
+		cdOfferings.add( cdOffering );
+		MeritBank.setCDOfferings( cdOfferings.toArray( new CDOffering[0] ) );
 		return cdOffering;
 	}
-	
-	
-	@GetMapping(value ="/CDOfferings")
-	public CDOffering[] getCDOfferings() {
-		return cdOfferings.toArray(new CDOffering[0]);
+
+	@GetMapping( value = "/CDOfferings" )
+	public CDOffering[] getCDOfferings()
+	{
+		return cdOfferings.toArray( new CDOffering[0] );
 	}
-	
 
 }
