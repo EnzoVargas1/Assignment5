37c243f4a6107ef62b2ddc17e9f4a85fa2e1dc30
ashwin nirmul da21-01-6311 <ashwin.metropcs@gmail.com>
2021-04-19 11:15:11 -0400

    WIP

--- src/main/java/com/meritamerica/assignment5/models/CheckingAccount.java
+++ src/main/java/com/meritamerica/assignment5/models/CheckingAccount.java
@@ -3,34 +3,36 @@ package com.meritamerica.assignment5.models;
 import java.util.Date;
 import java.util.StringTokenizer;
 
-public class CheckingAccount extends BankAccount {
-	
+public class CheckingAccount extends BankAccount
+{
 
-	public CheckingAccount() {
-		super(nextAccountNumber++, 0, CHECKING_INTERESTRATE, new Date());
+	public CheckingAccount()
+	{
+		super( nextAccountNumber++, 0, CHECKING_INTERESTRATE, new Date() );
 	}
-	public CheckingAccount(double balance) {
-		super(nextAccountNumber++, balance, CHECKING_INTERESTRATE, new Date());
+
+	public CheckingAccount( double balance )
+	{
+		super( nextAccountNumber++, balance, CHECKING_INTERESTRATE, new Date() );
 	}
-	public CheckingAccount(long accountNumber, double balance, double interestRate,java.util.Date accountOpenedOn) {
-		super(accountNumber,balance,interestRate,accountOpenedOn);
+
+	public CheckingAccount( long accountNumber, double balance, double interestRate, java.util.Date accountOpenedOn )
+	{
+		super( accountNumber, balance, interestRate, accountOpenedOn );
 	}
-	
-	public static CheckingAccount readFromString(String accountData) throws java.lang.NumberFormatException{
-		StringTokenizer token = new StringTokenizer(accountData, ",");
-		int numAccount = Integer.parseInt(token.nextToken());
-		double balance = Double.parseDouble(token.nextToken());
-		double rate = Double.parseDouble(token.nextToken());
-		Date date = new Date(token.nextToken());
-		CheckingAccount checking = new CheckingAccount(numAccount, balance, rate, date);
-		System.out.println("checkingbalance: "+balance);
+
+	public static CheckingAccount readFromString( String accountData ) throws java.lang.NumberFormatException
+	{
+		StringTokenizer token = new StringTokenizer( accountData, "," );
+		int numAccount = Integer.parseInt( token.nextToken() );
+		double balance = Double.parseDouble( token.nextToken() );
+		double rate = Double.parseDouble( token.nextToken() );
+		Date date = new Date( token.nextToken() );
+		CheckingAccount checking = new CheckingAccount( numAccount, balance, rate, date );
+		System.out.println( "checkingbalance: " + balance );
 		return checking;
 	}
-	
-	public static final double CHECKING_INTERESTRATE= 0.0001;
+
+	public static final double CHECKING_INTERESTRATE = 0.0001;
 	private static long nextAccountNumber = 0;
-
 }
-
-
-
